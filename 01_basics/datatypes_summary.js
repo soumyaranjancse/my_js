bbc82f37650639544808ef7a9416adc61e60052f
Soumya ranjan Behera <118543370+soumyaranjancse@users.noreply.github.com>
2025-10-03 16:54:40 +0000

    summary of datatypes

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,19 @@
+// Primitive //
+
+// 7 types : String , Number , Boolean, null, undefined , bigInt
+
+// reference ( non-primitive) //
+
+// Array, Objects, Functions 
+
+let arrays = ["Soumya","Ranjan","Behera"]
+
+let myObj = {
+    name:"Soumya",
+    age:18
+}
+
+let myFunction = function(){
+    console.log("Soumya");
+    
+}
